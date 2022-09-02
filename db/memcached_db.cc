840f98c4fcf9e8006e8cf49d57043c4b8e3b1ffc
yihan <yangyihan1998@gmail.com>
2022-09-02 13:09:52 +0800

    clear print

--- db/memcached_db.cc
+++ db/memcached_db.cc
@@ -21,11 +21,12 @@ int MemcachedDB::Read(const std::string &table, const std::string &key,
 
   retrieved_value = memcached_get(memc, key.c_str(), key.length(), &value_length, &flags, &rc);
 
-  if (rc == MEMCACHED_SUCCESS) {
-    cout << "Key retrieved successfully" << endl;
-    cout << "The key " << key << " returned value " << retrieved_value << endl;
-    free(retrieved_value);
-  } else {
+  // if (rc == MEMCACHED_SUCCESS) {
+  //   cout << "Key retrieved successfully" << endl;
+  //   cout << "The key " << key << " returned value " << retrieved_value << endl;
+  //   free(retrieved_value);
+  // } else {
+  if (rc != MEMCACHED_SUCCESS) {
     cout << "Couldn't retrieve key: " << memcached_strerror(memc, rc) << endl;
   }
   return DB::kOK;
@@ -37,9 +38,10 @@ int MemcachedDB::Update(const string &table, const string &key,
 
   for (KVPair &p : values) {
     rc = memcached_set(memc, key.c_str(), key.length(), p.second.c_str(), p.second.length(), (time_t)0, (uint32_t)0);
-    if (rc == MEMCACHED_SUCCESS) {
-      cout << "Key stored successfully! key " << key << ", value: " << p.second << endl;
-    } else {
+    // if (rc == MEMCACHED_SUCCESS) {
+    //   cout << "Key stored successfully! key " << key << ", value: " << p.second << endl;
+    // } else {
+    if (rc != MEMCACHED_SUCCESS) {
       cout << "Couldn't update key: " << memcached_strerror(memc, rc) << endl;
     }
   }
@@ -49,9 +51,10 @@ int MemcachedDB::Update(const string &table, const string &key,
 int MemcachedDB::Delete(const std::string &table, const std::string &key) {
   memcached_return rc;
   rc = memcached_delete(memc, key.c_str(), key.length(), (time_t)0);
-  if (rc == MEMCACHED_SUCCESS) {
-    cout << "Key deleted successfully" << endl;
-  } else {
+  // if (rc == MEMCACHED_SUCCESS) {
+  //   cout << "Key deleted successfully" << endl;
+  // } else {
+  if (rc != MEMCACHED_SUCCESS) {
     cout << "Couldn't delete key: " << memcached_strerror(memc, rc) << endl;
   }
   return DB::kOK;
